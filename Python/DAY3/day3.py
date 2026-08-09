ad3174188ba0e464ca4e8c75eaa2abb60f9552aa
nodecode22 <saurabhdoforyou@gmail.com>
2026-08-09 13:35:19 +0530

    just trying to use git

--- Python/DAY3/day3.py
+++ Python/DAY3/day3.py
@@ -1,47 +1,47 @@
-# # string 
+# string 
 
-# str1="Python"
-# str2="I love "
+str1="Python"
+str2="I love "
 
-# print(str2+str1)
-# print(len(str1))
+print(str2+str1)
+print(len(str1))
 
-# # indexing in string
-# word="python is best language"
-# # p-0,y-1,t-2,h-3,o-4,n-5
+# indexing in string
+word="python is best language"
+# p-0,y-1,t-2,h-3,o-4,n-5
 
-# # #lets suppose we have get the 4th letter in the string
+# #lets suppose we have get the 4th letter in the string
 
-# # print(word[3])
+# print(word[3])
 
-# # for ch in word:
-# #     print(ch)
+# for ch in word:
+#     print(ch)
 
-# #slicing the string
+#slicing the string
 
-# #syntax for slicing 
-# # str[start_indx:end_indx:step_indx]
+#syntax for slicing 
+# str[start_indx:end_indx:step_indx]
 
-# print(word[0:14])
+print(word[0:14])
 
-# print(word[3:])
+print(word[3:])
 
-# print(word[::-1])
+print(word[::-1])
 
-# # formatting in python
+# formatting in python
 
-# a=5
-# b=10
-# sum=a+b
+a=5
+b=10
+sum=a+b
 
-# # normal formatting
-# print("Language is {}".format("python"))
-# print("Sum of {} & {} is {}".format(a,b,sum))
+# normal formatting
+print("Language is {}".format("python"))
+print("Sum of {} & {} is {}".format(a,b,sum))
 
-# # index based formatting
-# print("Sum of {1} & {0} is {2}".format(a,b,sum))
+# index based formatting
+print("Sum of {1} & {0} is {2}".format(a,b,sum))
 
-# #value based formatting 
-# print("value of vars {a}".format(a=2))
+#value based formatting 
+print("value of vars {a}".format(a=2))
 x=12
 print(f"the value of x is {x}")
