e80c6fba768a7a7820635beadfc5dba03af83dbd
nodecode22 <saurabhdoforyou@gmail.com>
2026-08-10 17:32:30 +0530

    assignment 3 done

--- Python/DAY3/assignment_3.py
+++ Python/DAY3/assignment_3.py
@@ -1,129 +1,191 @@
-#1 Ask the user for a string and check whether it is a palindrome or not. A palindrome is a string which is same when we read it forward & backward. Eg - “madam”, “racecar” etc.
+# #1 Ask the user for a string and check whether it is a palindrome or not. A palindrome is a string which is same when we read it forward & backward. Eg - “madam”, “racecar” etc.
 
-x=input("Enter a string to check whether it is palindrome or not: ")
-s=[]
-for i in x:
-    s.append(i)
+# x=input("Enter a string to check whether it is palindrome or not: ")
+# s=[]
+# for i in x:
+#     s.append(i)
 
-s.reverse()
-new_string=""
-for i in s:
-    new_string+=i
+# s.reverse()
+# new_string=""
+# for i in s:
+#     new_string+=i
 
-if x==new_string:
-    print("This is a palindroome string!!")
-else:
-    print("This is not a palindrome string!!")
-
-
-#2. Given a list of integers compute the average of all numbers in the list
-
-list1=[12,2,22,4,57,8,97]
-sum=0
-for i in list1:
-    sum+=i
-print(f"the Average of elements of the list is {sum/len(list1)}")
+# if x==new_string:
+#     print("This is a palindroome string!!")
+# else:
+#     print("This is not a palindrome string!!")
 
 
-#3.  Input two lists of integers from the user. Merge them into one list and sort the result.
-q=int(input("Enter number of elements: "))
+# #2. Given a list of integers compute the average of all numbers in the list
 
-x1=[]
-x2=[]
-
-for i in range(0,q):
-    print(f"Enter the value for list1 element at index {i}")
-    c=int(input("Enter the value: "))
-    x1.append(c)
-
-for i in range(0,q):
-    print(f"Enter the value for list2 element at index {i}")
-    c=int(input("Enter the value: "))
-    x2.append(c)
-
-for i in x2:
-    x1.append(i)
-
-x1.sort()
-print(x1)
-
-# Given a tuple of integers, create:
-# • A tuple of all even numbers
-# • A tuple of all odd numbers
-
-t1=(1,2,3,4,5,6,7,8,9,10)
-
-t_even=()
-t_odd=()
-
-for num in t1:
-    if num % 2 == 0:
-        t_even += (num,)
-    else:
-        t_odd += (num,)
-
-print("Even numbers:", t_even)
-print("Odd numbers:", t_odd)
+# list1=[12,2,22,4,57,8,97]
+# sum=0
+# for i in list1:
+#     sum+=i
+# print(f"the Average of elements of the list is {sum/len(list1)}")
 
 
-# #Create a dictionary where:
-# • Keys = student names
-# • Values = marks (integer)
-# Write a menu-based program where user presses a key (’A’, ‘B’, ‘C’, ‘D’)
-# depending on the operation they want to perform on the dictionary:
-# 1. A - Add a student
-# 2. B - Update marks
-# 3. C - Search for a student
-# 4. D - Display all students and marks
+# #3.  Input two lists of integers from the user. Merge them into one list and sort the result.
+# q=int(input("Enter number of elements: "))
 
-student={}
+# x1=[]
+# x2=[]
 
-def Add_student():
-    name=input("Enter the name: ")
-    marks=int(input("Enter marks: "))
-    student.update({name:marks})
+# for i in range(0,q):
+#     print(f"Enter the value for list1 element at index {i}")
+#     c=int(input("Enter the value: "))
+#     x1.append(c)
+
+# for i in range(0,q):
+#     print(f"Enter the value for list2 element at index {i}")
+#     c=int(input("Enter the value: "))
+#     x2.append(c)
+
+# for i in x2:
+#     x1.append(i)
+
+# x1.sort()
+# print(x1)
+
+# # Given a tuple of integers, create:
+# # • A tuple of all even numbers
+# # • A tuple of all odd numbers
+
+# t1=(1,2,3,4,5,6,7,8,9,10)
+
+# t_even=()
+# t_odd=()
+
+# for num in t1:
+#     if num % 2 == 0:
+#         t_even += (num,)
+#     else:
+#         t_odd += (num,)
+
+# print("Even numbers:", t_even)
+# print("Odd numbers:", t_odd)
+
+
+# # #Create a dictionary where:
+# # • Keys = student names
+# # • Values = marks (integer)
+# # Write a menu-based program where user presses a key (’A’, ‘B’, ‘C’, ‘D’)
+# # depending on the operation they want to perform on the dictionary:
+# # 1. A - Add a student
+# # 2. B - Update marks
+# # 3. C - Search for a student
+# # 4. D - Display all students and marks
+
+# student={}
+
+# def Add_student():
+#     name=input("Enter the name: ")
+#     marks=int(input("Enter marks: "))
+#     student.update({name:marks})
     
 
-def marks_updt():
-    name=input("enter the student name whose marks you wanna change: ")
-    if name in student:
-        marks=int(input("Enter the marks: "))
-        student[name]=marks
+# def marks_updt():
+#     name=input("enter the student name whose marks you wanna change: ")
+#     if name in student:
+#         marks=int(input("Enter the marks: "))
+#         student[name]=marks
         
+#     else:
+#         print("Sorry this entity does not exist!!")
+        
+# def search():
+#     name=input("enter the student name to check: ")
+#     if name in student:
+#         print(f"the stduent {name} having marks {student[name]}")
+        
+#     else:
+#         print("Sorry this stduent does not exist!!")
+        
+# def display():
+#     print(student.items())
+#     print("")
+# while True:
+#     print("A - Add a student\nB - Update marks\nC - Search for a student\nD - Display all students and marks")
+#     value=input("Enter the option:(A,B,C,D): ")
+#     if (value=='A'or value=='a'):
+#         Add_student()
+        
+#     elif (value=='B' or value=='b'):
+#         marks_updt()
+        
+#     elif (value=='c' or value=='C'):
+#         search()
+        
+#     elif (value=='d' or value=='D'):
+#         display()
+        
+#     else:
+#         print("Wrong input Try again")
+#         continue
+#     x=input("Do you want to continue?(Y/N): ")
+#     if(x=='Y' or x=='y'):
+#         continue
+#     else:
+#         break
+
+
+# # 6. Given a list of words:
+# words = ["apple", "banana", "kiwi", "cherry", "mango"]
+# # Create a dictionary that maps each word to its length.
+# # Example:
+# # {"apple": 5, "banana": 6, "kiwi": 4, ...}
+
+# dict={}
+
+# for i in words:
+#     dict.update({i:len(i)})
+# print(dict.items())
+
+# # Write a program that takes a string from the user and prints the number of spaces in the string.
+
+# strin=input("enter the string: ")
+# space=0
+# for i in strin:
+#     if i==" ":
+#         space+=1
+# print(f"the number of spaces: {space}")
+
+# Write a program to check whether two lists share no common elements.
+# share no common elements 
+# share common elements list1 = [1, 2, 3] list2 = [3, 4]
+list1 = [1, 2, 3, 4] 
+list2 = [5, 6, 7, 8]
+
+x1=set(list1)
+x2=set(list2)
+
+if x1.intersection(x2)==set():
+    print("There is no common element in both list")
+else:
+    print(f"Common elements :{x1.intersection(x2)}")
+
+
+# Given a list, print all elements that appear more than once in the list.
+numbers = [1, 2, 3, 2, 4, 5, 1, 3, 3]
+
+seen=set()
+duplicates=set()
+
+for i in numbers:
+    if i in seen:
+        duplicates.add(i)
     else:
-        print("Sorry this entity does not exist!!")
-        
-def search():
-    name=input("enter the student name to check: ")
-    if name in student:
-        print(f"the stduent {name} having marks {student[name]}")
-        
-    else:
-        print("Sorry this stduent does not exist!!")
-        
-def display():
-    print(student.items())
-    print("")
-while True:
-    print("A - Add a student\nB - Update marks\nC - Search for a student\nD - Display all students and marks")
-    value=input("Enter the option:(A,B,C,D): ")
-    if (value=='A'or value=='a'):
-        Add_student()
-        
-    elif (value=='B' or value=='b'):
-        marks_updt()
-        
-    elif (value=='c' or value=='C'):
-        search()
-        
-    elif (value=='d' or value=='D'):
-        display()
-        
-    else:
-        print("Wrong input Try again")
-        continue
-    x=input("Do you want to continue?(Y/N): ")
-    if(x=='Y' or x=='y'):
-        continue
-    else:
-        break
+        seen.add(i)
+print(duplicates)
+
+#Ask the user for a string and print:
+# • All unique characters
+# • The count of unique characters
+
+text = input("Enter a string: ")
+
+unique = set(text)
+
+print("Unique characters:", unique)
+print("Count of unique characters:", len(unique))
+
