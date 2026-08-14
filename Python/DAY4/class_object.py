509aca177e3905a2ddd4cebe7a6c7ce4077074f6
nodecode22 <saurabhdoforyou@gmail.com>
2026-08-15 00:06:56 +0530

    more about class and object

--- Python/DAY4/class_object.py
+++ Python/DAY4/class_object.py
@@ -10,10 +10,18 @@ print(stu2.college, stu2.year)
 
 
 class Teacher:
-    def __init__(self,name,subject,salary):
+
+    def __init__(self): #this is a default constructor
+        print("Default constructor has been called!!")
+    #creating constructor in class teacher
+    def __init__(self,name,subject,salary): # it is a parameterized constructor
         self.name=name
         self.subject=subject
         self.salary=salary
-
+    #creating new constructor which will retun teacher salary
+    def get_salary(self):
+        return self.salary 
 tech1=Teacher("saurabh","English",50000)
+
+print(f"{tech1.name} is getting paid a salary of {tech1.get_salary()}")
 
